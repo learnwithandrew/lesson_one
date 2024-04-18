9ff61289d1d10eaf593526765e6ca56b81be05cc
andyjohn23 <andrewnick906@gmail.com>
2024-04-18 07:24:01 +0300

    add lesson content

---
+++ index.js
@@ -0,0 +1,32 @@
+// var age = 20;
+// let personName = "Andrew";
+// const location = "Nairobi";
+
+// console.log(age);
+// console.log(personName);
+// console.log(location);
+
+// function showAge() {
+//     var age = 16;
+//     if (age > 18) {
+//         console.log("You are an adult!")
+//     } else if (age >= 15) {
+//         console.log("You are a junior")
+//     } else {
+//         console.log("you are a toddler")
+//     }
+// }
+// showAge();
+
+
+// (function() {
+//     console.log('anonymous function')
+// }, console.log('anonymous function'))
+
+// function expressions
+// const anonymousFunction = function () {
+//     console.log('check test')
+// }
+
+// anonymousFunction();
+
